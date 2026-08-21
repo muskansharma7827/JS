a0015287f812c18da48bec36e56fd02dd698d117
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2026-08-21 11:12:19 +0530

    2nd day of learning js

---
+++ js_lecture2/lecture2.js
@@ -0,0 +1,75 @@
+//var name = "Muskan";
+//if(true){
+//    console.log(name);
+//}
+
+//function solve(){
+//    var name = "Muskan";
+//    console.log(name);
+//}
+//solve();
+
+//function solve2(){
+//    var age = 90;
+//    console.log(age);
+//}
+//solve2();
+
+//let age = 89;
+//console.log(age);
+
+
+//if(true){
+//    let age = 89;
+
+//}
+//console.log(age);
+
+
+//function solve3(){
+//    let age = 89;
+//    console.log(age);
+
+//}
+//solve3();
+
+//let age = 89;
+//if(true){
+//    console.log(age);
+//}
+
+//let age = 89;
+//let age = 90;
+
+//let age = 89;
+//age = 90;
+
+//const c = "animal";
+//console.log(c);
+
+//const c = "animal";
+//c = "plant";
+
+//const c = "animal";
+//const c;
+
+//let age;
+//console.log(age);
+
+/*let age = null;
+console.log(age);
+
+
+console.log(Number.MAX_SAFE_INTEGER);  // 9007199254740991
+console.log(Number.MIN_SAFE_INTEGER);  // -9007199254740991
+
+let x = 9007199254740991;
+console.log(x + 1); // 9007199254740992
+console.log(x + 2); // 9007199254740992 (⚠️ precision lost!)
+
+let big = 9007199254740991n;
+console.log(big + 2n); // 9007199254740993n
+*/
+let age = 89;
+console.log(typeof(age));
+
