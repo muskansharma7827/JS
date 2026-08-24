2ca8448c9789a9521cf542255e7000aa2ce139de
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2026-08-24 10:50:05 +0530

    3rd day of learning js 1st part

---
+++ js_lecture3/lecture3.js
@@ -0,0 +1,102 @@
+/*console.log("lecture3");
+
+let a = 2;
+let b =3;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);
+
+
+/*let x = 5;
+
+console.log(x++); //5
+console.log(++x); //7
+console.log(--x); //6
+console.log(x--); //6
+console.log(x);   //5
+
+
+console.log(10>5);
+console.log(10<5);
+console.log(10>=5);
+console.log(10<=5);
+console.log(10==5);    //"10 5 ke equal h?"  false
+console.log(10!=5);
+console.log(10===5);    // false
+console.log(10!==5);
+console.log(5==5);      //true
+console.log(5===5);     //true
+console.log('5'==5);     //true
+console.log('5'===5);    //false
+console.log(5 !== 5);
+
+
+let age = 89;
+let person_age =  (age>18) ? 'vote' : 'cant vote' ;
+console.log(person_age);
+
+
+let example = (true && true && true);   //true
+console.log(example);
+
+example = (true && true && false);      //false
+console.log(example);
+
+let ques = (true || true || true);       //true
+console.log(ques);
+
+ques = (true && true || true);               //true
+console.log(ques);
+
+ques = (false && true && true);       //false
+console.log(ques);
+
+ques = (true && false && true);           //false
+console.log(ques);
+
+ques = !(true);
+console.log(ques);
+ques = !(false);
+console.log(ques);
+
+
+let example = ('Muskan' && true);
+console.log(example);
+
+example = ('' && false);
+console.log(example);
+
+
+console.log(2 & 5);
+console.log(2 | 5);
+console.log(~(0));
+console.log(5 >> 2);    //5 divides by 2 power of 2
+console.log(5 << 2);    //5 multuplies 2 power of 2
+
+
+
+let age = 18;
+if(age > 18){
+    console.log("can vote");
+}
+else{
+    console.log("cannot vote");
+}
+*/
+
+let num=2;
+switch(num){
+    case 1 : console.log('hi');
+    break;
+    case 2 : console.log('hello');
+    break;
+    case 3 : console.log('hey');
+    break;
+    default: console.log('hiii');
+}
+
+
+   
