608fa701f8f094218bd0c89bfaac4ff761e20509
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2026-08-27 11:38:11 +0530

    5th day of learning js 2nd part

---
+++ js_lecture6/lecture6.js
@@ -0,0 +1,24 @@
+/*
+let obj = {
+    name : "muskan",
+    gender : "female",
+    "full name" : " muskan sharma",  
+    greet : function(){
+        console.log("Goodmorning everybody")
+        }
+
+}
+console.log(obj);
+obj.greet();
+console.log(typeof(obj))
+
+
+let arr = [ "hii" , "everybody" , 1, 3.15];
+console.log(arr);
+let arr1 = new Array("hii" , "everybody" , 1, 3.15);       //array constructor
+console.log(arr1);
+*/
+
+let arr2= ["welcome" , "to" , "this", "beautiful" , "world"];    //accessing the elements of array
+console.log(arr2[3]);
+
