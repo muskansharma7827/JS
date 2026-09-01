2c9cc75b9f3e26000bde5ca5afee507387364dec
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2026-09-01 11:03:47 +0530

    7th day lof learning js 2nd part

---
+++ js_lecture7/lecture7.js
@@ -0,0 +1,30 @@
+/*
+sayhello("Hey!");
+
+function sayhello(greeting){
+    console.log(greeting);
+}
+
+console.log(age);
+var age = 22;
+
+console.log(age);
+let age = 22;
+
+console.log(age);
+const age = 22;
+
+
+sayhello("Hey!");
+let sayhello = function(){
+    console.log("Hey!");
+}
+*/
+
+const obj1 = new house();
+class house{
+
+}
+
+
+
