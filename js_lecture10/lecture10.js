1e0e50e75729f0eaaed45c8cc9bee49478086eb2
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2026-09-02 12:43:39 +0530

    8th day of learning js 3rd part

---
+++ js_lecture10/lecture10.js
@@ -0,0 +1,28 @@
+/*
+console.log(Math.PI);
+console.log(Math.max(10, 20, 30));
+console.log(Math.min(10, 20, 30));
+console.log(Math.pow(2, 3));
+console.log(Math.round(4.7));
+console.log(Math.round(4.4));
+console.log(Math.floor(4.7));
+console.log(Math.ceil(4.4));
+console.log(Math.abs(-10));
+console.log(Math.random());    //o inclusive and 1 exclusive only give numbers b/w 0 and 1
+console.log(Math.sqrt(4));
+console.log(Math.PI);
+*/
+
+let curr = new Date();
+console.log(curr);
+
+let date = new Date('12,January,2004,4:00');
+console.log(date);
+
+console.log(date.getDate());
+
+console.log(date.getFullYear());
+
+console.log(date.setMonth(0));
+console.log(date);
+console.log(date.getDay());
