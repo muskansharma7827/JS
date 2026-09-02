b343048bcea162d31382edc094ba65c80bd22305
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2026-09-02 10:25:39 +0530

    8th day of learning js 1st part

---
+++ js_lecture8/lecture8.js
@@ -0,0 +1,137 @@
+/*
+//global scope
+var age = 22;
+console.log(age);
+
+{
+    console.log(age);
+    
+}
+
+function sayhello(){
+    console.log(age);
+}
+sayhello();
+
+if(5 > 2){
+    console.log(age);
+}
+
+for(let i=0;i<10;i++){
+    console.log(age);
+}
+    
+//global scope
+let age1 = 22;
+console.log(age);
+
+{
+    console.log(age);
+    
+}
+
+function sayhello(){
+    console.log(age);
+}
+sayhello();
+
+if(5 > 2){
+    console.log(age);
+}
+
+for(let i=0;i<10;i++){
+    console.log(age);
+}
+    
+
+//global scope
+const age2 = 22;
+console.log(age);
+
+{
+    console.log(age);
+    
+}
+
+function sayhello(){
+    console.log(age);
+}
+sayhello();
+
+if(5 > 2){
+    console.log(age);
+}
+
+for(let i=0;i<10;i++){
+    console.log(age);
+}
+*/
+
+/*
+console.log(age);
+function sayhello(){
+    const age = 22;
+    
+}
+
+sayhello();
+*/
+
+/*
+{
+    var age = 22;
+    console.log(age);
+}
+{
+    let age = 22;
+    console.log(age);
+}
+{
+    const age = 22;
+    console.log(age);
+}
+
+
+
+ console.log(age);
+{
+    var age = 22;
+   
+}
+
+ console.log(age);
+{
+    let age = 22;
+   
+}
+
+ console.log(age);
+{
+    const age = 22;
+   
+}
+    
+
+console.log(age);
+{
+    var age =22;
+}
+
+
+
+console.log(age);
+var age = 22;                    //undefined
+
+console.log(age);
+let age = 22;                      // cannot access age before initialization
+
+
+console.log(age);
+const age = 22;                  // cannot access age before initialization
+*/
+
+console.log(age);
+console.log("hello");
+console.log("people");
+let age = 22;
+console.log(age);
