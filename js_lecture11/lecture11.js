9c2390df49cc9cb932f774fefbc2625713f048b4
muskanjangi <163982489+muskanjangi@users.noreply.github.com>
2026-09-02 13:06:45 +0530

    8th day of learning js 4th part

---
+++ js_lecture11/lecture11.js
@@ -0,0 +1,26 @@
+let src = {
+    age:22,
+    height:180
+};
+
+/*let dest = {...src};
+console.log(src);
+dest.age = 23;
+console.log(dest);
+
+
+let dest = src;
+dest.age = 23;
+console.log(src);
+
+            console.log(dest);
+*/
+
+/*
+let dest = Object.assign({},src);
+console.log(src);
+dest.age = 23;
+console.log(dest);
+*/
+
+
